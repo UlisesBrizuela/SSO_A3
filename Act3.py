b1b0160c509994b2c8732d19297235f3b82e4944
Israel <ulises.brizuela2928@alumnos.udg.mx>
2023-03-06 21:59:46 -0600

    Round Robin terminado

--- Act3.py
+++ Act3.py
@@ -1,12 +1,47 @@
 #Actividad 3 - Seminario de solucion de problemas de sistemas operativos
 #Brizuela Arias Ulises Israel
-import os
-import sys
-from time import sleep
+
+from os import system 
+from sys import exit
+from collections import deque
+
+def LeerArchivo():
+    procesos = []
+    with open("./procesos.txt", 'r') as f:
+        lineas = f.readlines()
+        for linea in lineas:
+            proceso, ciclos, prioridad = linea.strip().split(',')
+            procesos.append((proceso, int(ciclos), int(prioridad)))
+    return procesos
 
 
-def menu(): 
-    os.system('cls')
+
+def RoundRobin(procesos, Q):
+    system('cls')
+    cola = deque(procesos)
+    while cola:
+        proceso_actual = cola.popleft()
+        nombre, quantum, p = proceso_actual
+        print(f"Ejecuntando: {nombre}, con {quantum} quantum.")
+        quantum -= Q
+        if quantum > 0:
+            cola.append((nombre, quantum, p))
+            print(f"El proceso: {nombre} volvio a la cola con {quantum} quantum restantes.\n")
+        else:
+            print(f"Proceso {nombre} terminado.\n")
+    input("presiona Enter para continuar")
+    Menu()
+
+def SFJ():
+    system('cls')
+
+
+
+
+
+def Menu(): 
+    system('cls')
+    procesos = LeerArchivo()
     try:
         m = int(input("""
         SELECCIONA UNA OPCION.
@@ -17,10 +52,10 @@ def menu():
         4.- PRIORIDADES
         0.- SALIR
         
-        : """))
+        # """))
 
         if m == 1: 
-            print("opcion 1")
+            RoundRobin(procesos, 3)
         elif m == 2: 
             print("opcion 2")
         elif m == 3: 
@@ -28,18 +63,18 @@ def menu():
         elif m == 4: 
             print("opcion 4")
         elif m == 0:
-            os.system('cls')
-            sys.exit();
+            system('cls')
+            exit();
         else:
             print("Opcion Invalida")
-            sleep(1)
-            menu()
+            input("presiona Enter para continuar")
+            Menu()
 
     except ValueError: 
         print("Dato invalido")
-        sleep(1)
-        menu()
+        input("presiona Enter para continuar")
+        Menu()
 
 
 if __name__ == "__main__":
-    menu()
+    Menu()
